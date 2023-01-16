0a9f50e6d7d6bdc3b4497540eba9f245e3118455
Neurs <31346263+Neurs12@users.noreply.github.com>
2023-01-16 17:00:56 +0700

    Delete screen_manager.dart

--- updater/screen_manager.dart
+++
@@ -1,9 +0,0 @@
-import 'screens/landing.dart';
-import 'screens/scores.dart';
-
-class Screen {
-  var landing = const LandingScreen();
-  dynamic scores() {
-    return const ScoresScreen();
-  }
-}
