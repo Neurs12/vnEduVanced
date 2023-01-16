3a8a9a7e56f3dbab1f35bdeb5cea96a2599edba1
Neurs <31346263+Neurs12@users.noreply.github.com>
2023-01-16 16:59:38 +0700

    Code revision
    Bug fixed, revision data approach.

---
+++ updater/screen_manager.dart
@@ -0,0 +1,9 @@
+import 'screens/landing.dart';
+import 'screens/scores.dart';
+
+class Screen {
+  var landing = const LandingScreen();
+  dynamic scores() {
+    return const ScoresScreen();
+  }
+}
